acb6d118f6fde7bb952c2fb0b24097330e7331b5
Felipe Pereira <luisfelipelfsp3@gmail.com>
2024-03-23 19:21:30 -0300

    :sparkles: feat: Routers and Async Caller

--- src/server/trpc/server.ts
+++ src/server/trpc/server.ts
@@ -1,9 +1,32 @@
-import { userRouter } from "./routers/user";
-import { router } from "./trpc";
+import authRouter from "./routers/auth";
+import { createContext } from "@/server/trpc/context";
+import { t, protectedProcedure } from "@/server/trpc/trpc";
+import { getMeHandler } from "./controllers/user-controller";
 
-export const appRouter = router({
-  user: userRouter,
-  
+const healthCheckerRouter = t.router({
+	healthchecker: t.procedure.query(() => {
+		return {
+			status: "success",
+			message: "Welcome to trpc with Next.js 14 and React Query",
+		};
+	}),
 });
+
+const userRouter = t.router({
+	getMe: protectedProcedure.query(({ ctx }) => getMeHandler({ ctx })),
+});
+
+export const appRouter = t.mergeRouters(
+	healthCheckerRouter,
+	authRouter,
+	userRouter
+);
+
+export const createCaller = t.createCallerFactory(appRouter);
+
+export const createAsyncCaller = async () => {
+	const context = await createContext();
+	return createCaller(context);
+};
 
 export type AppRouter = typeof appRouter;

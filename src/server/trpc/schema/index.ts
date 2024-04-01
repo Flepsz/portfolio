fc8ca351ece6924f9f1339399e589cd43c44f823
Felipe Pereira <luisfelipelfsp3@gmail.com>
2024-03-31 21:44:44 -0300

    :ok_hand:	style: Formating

--- src/server/trpc/schema/index.ts
+++ src/server/trpc/schema/index.ts
@@ -21,13 +21,13 @@ export const createUserSchema = object({
 });
 
 export const loginUserSchema = object({
-  email: string({ required_error: 'Email is required' })
-    .min(1, 'Email is required')
-    .email('Invalid email or password'),
-  password: string({ required_error: 'Password is required' }).min(
-    1,
-    'Password is required'
-  ),
+	email: string({ required_error: "Email is required" })
+		.min(1, "Email is required")
+		.email("Invalid email or password"),
+	password: string({ required_error: "Password is required" }).min(
+		1,
+		"Password is required"
+	),
 });
 
 export type CreateUserInput = TypeOf<typeof createUserSchema>;
